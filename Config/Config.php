77364a0a35143fee60b987daaafb41aa89372a7c
miwebdeprueba <pelisserver@gmail.com>
2025-07-11 01:21:49 -0500

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/shop/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "shop";
+const CHARSET = "charset=utf8";
+const TITLE = "SHOP";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+?>
